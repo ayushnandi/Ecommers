66e5528b1f9b8a9ed6b6a9a0df0ee1087b9a3025
unknown <ayushnandi>
2025-05-10 00:35:18 +0530

    first commit

---
+++ src/components/Header/Navigation/index.jsx
@@ -0,0 +1,56 @@
+import { Button } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+import { FaAngleDown } from "react-icons/fa";
+import React from 'react'
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
+import { Link } from 'react-router-dom';
+
+export default function Navigation() {
+  return (
+    <nav>
+      <div className="container flex items-center justify-end py-1 gap-6  ">
+        <div className="col1 w-[20%]">
+            <Button className="!text-black gap-[4px] w-full ">
+              <MenuIcon/> Shop By Categories <FaAngleDown className='ml-auto '/>
+            </Button>
+        </div>
+        <div className="col2 w-[60%] ">
+          <ul className="flex items-center justify-around gap-6 ">
+            <li>
+              <Link className="link text-[15px] font-[500] " to={"/"} >Home</Link>
+            </li>
+            <li>
+              <Link className="link text-[15px] font-[500] " to={"/"} >Groceries </Link>
+            </li>
+            <li>
+              <Link className="link text-[15px] font-[500] " to={"/"} >Beauty </Link>
+            </li>
+            <li>
+              <Link className="link text-[15px] font-[500] " to={"/"} >Wellness </Link>
+            </li>
+            <li>
+              <Link className="link text-[15px] font-[500] " to={"/"} >Fashion </Link>
+            </li>
+            <li>
+              <Link className="link text-[15px] font-[500] " to={"/"} >Fashion </Link>
+            </li>
+            <li>
+              <Link className="link text-[15px] font-[500] " to={"/"} >Electronics</Link>
+            </li>
+            <li>
+              <Link className="link text-[15px] font-[500] " to={"/"} >Bags</Link>
+            </li>
+            <li>
+              <Link className="link text-[15px] font-[500] " to={"/"} >Jewellery</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col3 w-[25%] ">
+          <div className='flex justify-end'>
+            <Button to={"/delivery"} className="link  !text-black font-[500] gap-3 " > <RocketLaunchIcon/> Free International Delivery</Button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Search from '../Search'
-// import ShoppingCartRoundedIcon from '@mui/icons-material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import EmailIcon from '@mui/icons-material/Email';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { Button } from '@mui/material';
+import Navigation from './Navigation/index';
+
 
 export default function Header() {
   return (
     <>
       <header>
-        <div className="top-stip py-1 border-y-[1px] border-gray-400">
+        <div className="top-stip py-1 border-y-[1px] border-gray-300">
             <div className="container">
                 <div className="flex items-center justify-between ">
                     <div className="col1 w-[50%]">
@@ -27,31 +32,36 @@ export default function Header() {
             </div>
         </div>
 
-        <div className="header">
-          <div className="container flex gap-3 items-center justify-between ">
+        <div className="header border-b-[1px] border-gray-300">
+          <div className="container  flex gap-3 items-center justify-between ">
             <div className="col1 w-[25%] flex justify- gap-3 py-2 ">
               <Link to={"/"}>
-                <img src="/logo.png" className="h-[80px] b-[80px] " alt="" />
+                <img src="/logo.png" className="h-[80px] w-[80px] " alt="" />
               </Link>
-              <p className="flex items-center logo-font font-[700] text-[35px] text-gray-700 ">
+              <p className="flex items-center logo-font font-[700] text-[32px] text-gray-700 ">
                 Shopping Cart
               </p>
             </div>
             <div className=" col2 w-[40%]  ">
               <Search/>
             </div>
-            <div className=" col3 w-[30%] flex items-center pl-7 ">
+            <div className=" col3 w-[30%] flex text-gray-600 items-center justify-around pl-7 ">
               <div className="flex items-center justify-around">
                 <div className="list-none">
-                  <Link to="/login" className='link transition text-[15px] font-[500] text ' >Login</Link> | &nbsp;
+                  <Link to="/login" className='link transition text-[15px] font-[500] ' >Login</Link> | &nbsp;
                   <Link to="/register" className='link transition text-[15px] font-[500] ' >Register</Link>
-                </div>              
-                <div className="item-list">
-                  
                 </div>
+              </div>
+              <div className="item-list flex items-center gap-5 justify-between  ">
+                <Button className=" !w-[37px] z-50 !min-w-[37px] h-[37px]  !text-gray-700 !rounded-full "><FavoriteBorderIcon className='link' /></Button>
+                <Button className=" !w-[37px] z-50 !min-w-[37px] h-[37px]  !text-gray-700 !rounded-full "><ShoppingCartIcon className='link' /></Button>
+                <Button className=" !w-[37px] z-50 !min-w-[37px] h-[37px]  !text-gray-700 !rounded-full "><EmailIcon className='link' /></Button>              
               </div>
             </div>
           </div>
+        </div>
+        <div className='border-b-[1px] border-gray-300'>
+          <Navigation/>
         </div>
 
       </header>
